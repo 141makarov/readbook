39a6a9bf34b6ce9e7cfd5d1ebeb5085ca12e2855
141makarov <38344896+141makarov@users.noreply.github.com>
2018-07-25 22:33:10 +0800

    更新微信爬取界面

--- 0725selectbook.py
+++ 0725selectbook.py
@@ -24,16 +24,18 @@ def judgeCommonts(html):
     soup = BeautifulSoup(html, 'lxml')
     li = soup.find_all('li', class_='comment-item')
     for i in li:
-        print(i.find(class_='comment-info').contents[3]['title'])
+            print(i.find(class_='comment-vote').text.replace(' ','').replace('\n',' '))
+            print(i.find(class_='comment-info').contents[3]['title'])
+        except:
+            continue
         print(i.find(class_='short').text)
         print('-----------------')
 
 def judgeDouban(html):
     soup = BeautifulSoup(html, 'lxml')
     tr = soup.find_all('div',class_='result')
-    for i in range(0, 5):
+    for i in range(0, 1):
         print(tr[i].find(class_='title').find('a').text.replace('\n', '').encode('GBK', 'ignore').decode('GBk'))
-        # print(tr[i].find(class_='title').find('a')['onclick'])
         print(tr[i].find(class_='rating-info').text.replace('\n', '').encode('GBK', 'ignore').decode('GBk'))
         s = tr[i].find(class_='title').find('a')['onclick']
         l1 = []
@@ -49,28 +51,25 @@ def judgeDouban(html):
 
 def judgepage(html):
     soup = BeautifulSoup(html, 'lxml')
-    tr = soup.find_all('strong')
+    pattern = re.compile('《(.*?)》')
+    result1 = re.findall(pattern, soup.text.encode('GBK', 'ignore').decode('GBk'))
     l1 = []
-    for i in tr:
-        try:
-            pattern = re.compile('《(.*?)》')
-            result1 = re.search(pattern, i.text)
-            l1.append(result1.group())
-        except:
-            print('')
+    for item in result1:
+        l1.append(item.replace('/n', ''))
     print(l1)
     return l1
-    # html_doc = str(html, 'utf-8')  # html_doc=html.decode("utf-8","ignore")
-    # print(html_doc)
-    # print(response.text.encode('utf-8'))
-    # return response.text
+
 
 if __name__ == '__main__':
-    url = 'https://mp.weixin.qq.com/s?__biz=MzA3NTE5NDEzMw==&mid=2653049306&idx=1&sn=35502c7bf58b8e645efd1e78213f48cf&chksm=84a2bba0b3d532b65e0990aece377658b149ae094c7aa656f11c876cdedc73a5c119edbf35b0&mpshare=1&scene=1&srcid=0725Ul4qPAKQpbJ0Avr4vtmD#rd'
+    # url = 'https://mp.weixin.qq.com/s?__biz=MzA3NTE5NDEzMw==&mid=2653049306&idx=1&sn=35502c7bf58b8e645efd1e78213f48cf&chksm=84a2bba0b3d532b65e0990aece377658b149ae094c7aa656f11c876cdedc73a5c119edbf35b0&mpshare=1&scene=1&srcid=0725Ul4qPAKQpbJ0Avr4vtmD#rd'
+    url = 'https://mp.weixin.qq.com/s?__biz=MzA3NTE5NDEzMw==&mid=2653049280&idx=1&sn=cb422b8006f0fb724f280c5c49bad7ca&chksm=84a2bbbab3d532ac9d1a0523c9b6497758bced81394c1648fb8be42b6ec7e60a734b87f1ae20&scene=0#rd'
     bookList = judgepage(getpage(url))
     for item in bookList:
         url_douban = 'https://www.douban.com/search?cat=1001&q='+ str(item)
         judgeDouban(getpage(url_douban))
+
+
+
 # url_Detaildouban = 'https://book.douban.com/subject/3879026'
 # judgeDetail(getpage(url_Detaildouban))
 # url_comments = 'https://book.douban.com/subject/3879026/comments/'
